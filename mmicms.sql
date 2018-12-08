9b73e34a993c2bfb0d1040fd01819b0dee57ebff
claw68 <franzandrew68@gmail.com>
2018-12-09 02:12:20 +0800

    Add db file

---
+++ mmicms.sql
@@ -0,0 +1,116 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.32 : Database - mmicms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mmicms` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mmicms`;
+
+/*Table structure for table `contestant` */
+
+DROP TABLE IF EXISTS `contestant`;
+
+CREATE TABLE `contestant` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) DEFAULT NULL,
+  `number` int(200) DEFAULT NULL,
+  `gender` varchar(200) DEFAULT NULL,
+  `picture` varchar(200) DEFAULT NULL,
+  `thumbnail` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `contestant` */
+
+/*Table structure for table `criteria` */
+
+DROP TABLE IF EXISTS `criteria`;
+
+CREATE TABLE `criteria` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `percentage` double NOT NULL,
+  `event` int(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `criteria` */
+
+insert  into `criteria`(`id`,`name`,`percentage`,`event`) values (2,'asdf',121,33);
+
+/*Table structure for table `event` */
+
+DROP TABLE IF EXISTS `event`;
+
+CREATE TABLE `event` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `enable` tinyint(1) NOT NULL DEFAULT '1',
+  `show` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `event` */
+
+insert  into `event`(`id`,`name`,`enable`,`show`) values (2,'asadf1',1,1);
+
+/*Table structure for table `score_cont_judge` */
+
+DROP TABLE IF EXISTS `score_cont_judge`;
+
+CREATE TABLE `score_cont_judge` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `contestant` int(200) NOT NULL,
+  `user` int(200) NOT NULL,
+  `score` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `score_cont_judge` */
+
+/*Table structure for table `score_event_criteria` */
+
+DROP TABLE IF EXISTS `score_event_criteria`;
+
+CREATE TABLE `score_event_criteria` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `event` int(200) NOT NULL,
+  `criteria` int(200) NOT NULL,
+  `score_cont_judge` int(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `score_event_criteria` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `alias` varchar(200) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `role` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`alias`,`name`,`role`) values (2,'admin','password','Administrator','Franz Andrew Vallente','Administrator'),(3,'rtflores','password','Tabulator','Rozanne Tuesday Flores','Tabulator'),(4,'judgea','password','Judge 1','Judge 1','Judge');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
